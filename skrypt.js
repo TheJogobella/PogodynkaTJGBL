7cee43526fa6b5497b7f35a9ee5146a7318d9c6d
Piotr Tomaszkiewicz <piotr.tomaszkiewicz@live.zs1mm.edu.pl>
2022-11-21 02:11:06 +0100

    kolo i kolor ramki

--- skrypt.js
+++ skrypt.js
@@ -8,8 +8,10 @@ var cords1y
 var cords2x
 var cords2y
 var lineW = 1
-var rbgcolor = document.getElementById("rgbcol").value
-var alfa = 255
+var rbgcolor = "#ffffff"
+var rbgcolorbg = "#ffffff"
+var alfa = 1
+var alfabg = 1
 function changeAlfa1(){
   alfa = document.getElementById("alfa1").value
   document.getElementById("alfa2").value = alfa
@@ -18,8 +20,19 @@ function changeAlfa2(){
   alfa = document.getElementById("alfa2").value
   document.getElementById("alfa1").value = alfa
 }
+function changeAlfa1bg(){
+  alfabg = document.getElementById("alfa1bg").value
+  document.getElementById("alfa2bg").value = alfabg
+}
+function changeAlfa2bg(){
+  alfabg = document.getElementById("alfa2bg").value
+  document.getElementById("alfa1bg").value = alfa
+}
 function changeColor(){
   rbgcolor = document.getElementById("rgbcol").value
+}
+function changeColorBg(){
+  rbgcolorbg = document.getElementById("rgbcolbg").value
 }
 function changeWidth(){
   lineW = document.getElementById("width").value
@@ -57,8 +70,10 @@ function activeLine(){
 }
 function squareDown(){
   ctx.beginPath();
-  ctx.fillStyle = rbgcolor
-  ctx.globalAlpha = alfa
+  ctx.strokeStyle = rbgcolor
+  ctx.fillStyle = rbgcolorbg
+  ctx.globalAlpha = alfabg
+  ctx.lineWidth = lineW
   cords1x =  cordsx
   cords1y =  cordsy
 }
@@ -66,11 +81,38 @@ function squareUp(){
   cords2x =  cordsx
   cords2y =  cordsy
   ctx.fillRect(cords1x, cords1y, cords2x-cords1x, cords2y-cords1y);
+  ctx.globalAlpha = alfa
+  ctx.rect(cords1x, cords1y, cords2x-cords1x, cords2y-cords1y)
+  ctx.stroke()
 }
 function activeSquare(){
   activeTool = "square"
   board.setAttribute("onmousedown", "squareDown()")
   board.setAttribute("onmouseup", "squareUp()")
+  board.setAttribute("onmousemove", "")
+  board.setAttribute("onclick", "")
+}
+function circleDown(){
+  ctx.beginPath();
+  ctx.strokeStyle = rbgcolor
+  ctx.fillStyle = rbgcolorbg
+  ctx.globalAlpha = alfabg
+  ctx.lineWidth = lineW
+  cords1x =  cordsx
+  cords1y =  cordsy
+}
+function circleUp(){
+  cords2x =  cordsx
+  cords2y =  cordsy
+  ctx.arc(cords1x, cords1y,  Math.sqrt(Math.pow((cords2x-cords1x), 2)+Math.pow((cords2y-cords1y), 2)), 0, 2 * Math.PI);
+  ctx.fill()
+  ctx.globalAlpha = alfa
+  ctx.stroke();
+}
+function activeCircle(){
+  activeTool = "circle"
+  board.setAttribute("onmousedown", "circleDown()")
+  board.setAttribute("onmouseup", "circleUp()")
   board.setAttribute("onmousemove", "")
   board.setAttribute("onclick", "")
 }
